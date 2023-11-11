b643b63871e96dd12e14908d8ba6b68aa624bb7b
FermendjinPatricia <patricia.fermendjin@student.upt.ro>
2023-11-12 00:46:22 +0200

    Proiect Saptamana 7

--- week6.c
+++ week6.c
@@ -10,16 +10,23 @@
 
 struct stat informatii;
 
+typedef enum type_of_file{
+    REGULAR,DIRECTOR,LINK
+} type_of_file;
+
 void testArgs(int argc, char *argv[]){
     if (argc != 2){
         printf("Usage %s\n",argv[1]);
         exit(-1);
     }
-    const char *ext = strrchr(argv[1], '.');
+}
+
+int checkBMPFile(char *name){
+    const char *ext = strrchr(name, '.');
     if (ext == NULL || strcmp(ext, ".bmp") != 0){
-        printf("Usage %s %s\n",argv[0],argv[1]);
-        exit(-1);
+        return 0;
     }
+    return 1;
 }
 
 int openFile(char *name){
@@ -53,9 +60,9 @@ int writeInBuffer(char *buffer, char *message, char *s){
     return sprintf(buffer,message,s);
 }*/
 
-void writeName(int fileDescriptorOut, char *nume){
+void writeName(int fileDescriptorOut, char *nume, char *type){
     char buffer[100];
-    int charBytes = sprintf(buffer,"nume fisier: %s\n",nume);
+    int charBytes = sprintf(buffer,"nume %s: %s\n",type, nume);
     if((write(fileDescriptorOut,buffer,charBytes)) == -1){
         perror("Nu s-a putut scrie numele fisierului.\n");
         exit(-1);
@@ -101,9 +108,9 @@ void writeHeight(int fileDescriptorIn, int fileDescriptorOut){
     }
 }
 
-void writeSize(int fileDescriptorOut, int size){
+void writeSize(int fileDescriptorOut, int size, char *type){
     char buffer[100];
-    int charBytes = sprintf(buffer,"dimensiune: %d\n",size);
+    int charBytes = sprintf(buffer,"dimensiune%s: %d\n", type, size);
     if((write(fileDescriptorOut,buffer,charBytes)) == -1){
         perror("Nu s-a putut scrie dimensiunea.\n");
         exit(-1);
@@ -162,10 +169,10 @@ void getUserAccessRights(mode_t mode, char *buffer){
     sprintf(buffer,"%c%c%c",readRights,writeRights,executeRights);
 }
 
-void writeUserAccessRights(int fileDescriptorOut, mode_t mode){
+void writeUserAccessRights(int fileDescriptorOut, mode_t mode, char *type){
     char buffer[100],bufferRights[50];
     getUserAccessRights(mode,bufferRights);
-    int charBytes = sprintf(buffer,"drepturi de acces user: %s\n", bufferRights);
+    int charBytes = sprintf(buffer,"drepturi de acces user%s: %s\n",type, bufferRights);
     if((write(fileDescriptorOut,buffer,charBytes)) == -1){
         perror("Nu s-au putut scrie drepturile de acces pentru user.\n");
         exit(-1);
@@ -180,10 +187,10 @@ void getGroupAccessRights(mode_t mode, char *buffer){
     sprintf(buffer,"%c%c%c",readRights,writeRights,executeRights);
 }
 
-void writeGroupAccessRights(int fileDescriptorOut, mode_t mode){
+void writeGroupAccessRights(int fileDescriptorOut, mode_t mode, char *type){
     char buffer[100],bufferRights[50];
     getGroupAccessRights(mode,bufferRights);
-    int charBytes = sprintf(buffer,"drepturi de acces grup: %s\n", bufferRights);
+    int charBytes = sprintf(buffer,"drepturi de acces grup%s: %s\n",type, bufferRights);
     if((write(fileDescriptorOut,buffer,charBytes)) == -1){
         perror("Nu s-au putut scrie drepturile de acces pentru grup.\n");
         exit(-1);
@@ -198,36 +205,100 @@ void getOthersAccessRights(mode_t mode, char *buffer){
     sprintf(buffer,"%c%c%c",readRights,writeRights,executeRights);
 }
 
-void writeOthersAccessRights(int fileDescriptorOut, mode_t mode){
+void writeOthersAccessRights(int fileDescriptorOut, mode_t mode, char *type){
     char buffer[100],bufferRights[50];
     getOthersAccessRights(mode,bufferRights);
-    int charBytes = sprintf(buffer,"drepturi de acces grup: %s\n", bufferRights);
+    int charBytes = sprintf(buffer,"drepturi de acces altii%s: %s\n",type, bufferRights);
     if((write(fileDescriptorOut,buffer,charBytes)) == -1){
-        perror("Nu s-au putut scrie drepturile de acces pentru grup.\n");
+        perror("Nu s-au putut scrie drepturile de acces pentru altii.\n");
         exit(-1);
     }
+}
+type_of_file typeOfFile(mode_t mode){
+    if(S_ISREG(mode)!=0) return REGULAR; //regular file
+    if(S_ISDIR(mode)!=0) return DIRECTOR; // director
+    if(S_ISLNK(mode)!=0) return LINK; //link
+    
+    return -1;
+    
 }
 
 int main(int argc,char *argv[]){
     testArgs(argc,argv);
-    int fileDescriptorIn = openFile(argv[1]);
-    int fileDescriptorOut = createFile("statistica.txt");
-
-    int stat_out=stat(argv[1],&informatii);
+    int stat_out=lstat(argv[1],&informatii);
     if(stat_out==-1)
     {
         perror("S-a produs o eroare la aflarea atributelor.");
         exit(-1);
     }
-    writeName(fileDescriptorOut, argv[1]);
-    writeWidth(fileDescriptorIn, fileDescriptorOut);
-    writeHeight(fileDescriptorIn, fileDescriptorOut);
-    writeSize(fileDescriptorOut,  informatii.st_size);
-    writeUserId(fileDescriptorOut, informatii.st_uid);
-    writeTime(fileDescriptorOut, informatii.st_mtime);
-    writeHardLinks(fileDescriptorOut,informatii.st_nlink);
-    writeUserAccessRights(fileDescriptorOut,informatii.st_mode);
-    writeGroupAccessRights(fileDescriptorOut,informatii.st_mode);
-    writeOthersAccessRights(fileDescriptorOut,informatii.st_mode);
+
+    type_of_file type = typeOfFile(informatii.st_mode);
+
+    switch (type)
+    {
+    case REGULAR:{
+            int fileDescriptorIn = openFile(argv[1]);
+            int fileDescriptorOut = createFile("statistica.txt");
+            writeName(fileDescriptorOut, argv[1],"fisier");
+            if(checkBMPFile(argv[1])==1) {
+                writeWidth(fileDescriptorIn, fileDescriptorOut);
+                writeHeight(fileDescriptorIn, fileDescriptorOut);
+            }
+            writeSize(fileDescriptorOut,  informatii.st_size," fisier");
+            writeUserId(fileDescriptorOut, informatii.st_uid);
+            writeTime(fileDescriptorOut, informatii.st_mtime);
+            writeHardLinks(fileDescriptorOut,informatii.st_nlink);
+            writeUserAccessRights(fileDescriptorOut,informatii.st_mode,"");
+            writeGroupAccessRights(fileDescriptorOut,informatii.st_mode,"");
+            writeOthersAccessRights(fileDescriptorOut,informatii.st_mode,"");
+            if(close(fileDescriptorIn)!=0){
+                perror("Nu s-a putut inchide fisierul.\n");
+                exit(-1);
+            }
+            if(close(fileDescriptorOut)!=0){
+                perror("Nu s-a putut inchide fisierul.\n");
+                exit(-1);
+            }
+            break;
+    }
+    case DIRECTOR:{
+            int fileDescriptorOut = createFile("statistica.txt");
+            writeName(fileDescriptorOut, argv[1],"director");
+            writeUserId(fileDescriptorOut, informatii.st_uid);
+            writeUserAccessRights(fileDescriptorOut,informatii.st_mode,"");
+            writeGroupAccessRights(fileDescriptorOut,informatii.st_mode,"");
+            writeOthersAccessRights(fileDescriptorOut,informatii.st_mode,"");
+            if(close(fileDescriptorOut)!=0){
+                perror("Nu s-a putut inchide fisierul.\n");
+                exit(-1);
+            }
+            break;
+    }
+    case LINK:{
+            int fileDescriptorOut = createFile("statistica.txt");
+            writeName(fileDescriptorOut, argv[1],"legatura");
+            struct stat informatiiFisierTarget;
+            int stat_link = stat(argv[1],&informatiiFisierTarget);
+            if(stat_link==-1)
+            {
+                perror("S-a produs o eroare la aflarea atributelor legaturii.");
+                exit(-1);
+            }
+            writeSize(fileDescriptorOut,informatii.st_size," legatura");
+            writeSize(fileDescriptorOut,informatiiFisierTarget.st_size," fisier");
+            writeUserAccessRights(fileDescriptorOut,informatii.st_mode," legatura");
+            writeGroupAccessRights(fileDescriptorOut,informatii.st_mode," legatura");
+            writeOthersAccessRights(fileDescriptorOut,informatii.st_mode," legatura");
+            if(close(fileDescriptorOut)!=0){
+                perror("Nu s-a putut inchide fisierul.\n");
+                exit(-1);
+            }
+            break;
+    }
+    default:{
+            printf("Nu se poate realiza scrierea in fisier in conditiile date.\n");
+            break;
+    }
+    }
     return 0;
 }
